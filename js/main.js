71b6147ea77043f1a747099252c07a330af0a998
Adam_gfb <vidam21042001@gmail.com>
2023-06-10 00:42:51 +0700

    code end homepage and interface productspage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-  $(".nav-link").click(function (e) {
-    e.preventDefault();
-    j;
-  });
+  function gotopage(selector) {
+    $(selector).click(function (e) {
+      e.preventDefault();
+      let link = $(this).attr("href");
+      window.location = link;
+    });
+  }
+
+  $(".back-home-page").attr("href", "index.html");
+  $(".logo").attr("href", "index.html");
+  gotopage(".nav-link.products-page");
+  gotopage(".logo");
+  gotopage(".back-home-page");
 });
